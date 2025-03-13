aerth/aquachain

commit 5a1deaefa569f2332346cf105c2c6d6f338885df

update

rlp/encode.go
@@ -111,6 +111,7 @@ func EncodeToBytes(val interface{}) ([]byte, error) {
 // Please see the documentation of Encode for the encoding rules.
 func EncodeToReader(val interface{}) (size int, r io.Reader, err error) {
 	eb := encbufPool.Get().(*encbuf)
+	defer encbufPool.Put(eb)
 	eb.reset()
 	if err := eb.encode(val); err != nil {
 		return 0, nil, err
@@ -182,6 +183,7 @@ func (w *encbuf) Write(b []byte) (int, error) {
 func (w *encbuf) encode(val interface{}) error {
 	rval := reflect.ValueOf(val)
 	ti, err := cachedTypeInfo(rval.Type(), tags{})
+	// log.Warn("encode", "type", rval.Type(), "kind", rval.Kind(), "ti", ti, "thing", fmt.Sprintf("%#v", val), "err", err)
 	if err != nil {
 		return err
 	}

core/genesis.go
@@ -159,7 +159,7 @@ func SetupGenesisBlock(db aquadb.Database, genesis *Genesis) (*params.ChainConfi
 	stored := GetCanonicalHash(db, 0)
 	if (stored == common.Hash{}) {
 		if genesis == nil {
-			genesis = DefaultGenesisBlock()
+			genesis = NewDefaultGenesisBlock()
 			log.Info("Writing default genesis block", "chain", genesis.Config.Name())
 		} else {
 			log.Info("Writing custom genesis block")
@@ -347,24 +347,27 @@ func GenesisBlockForTesting(db aquadb.Database, addr common.Address, balance *bi
 	return g.MustCommit(db)
 }
 
-func DefaultGenesisByName(name string) *Genesis {
-	switch name {
-	case "aqua":
-		return DefaultGenesisBlock()
-	case "testnet":
-		return DefaultTestnetGenesisBlock()
-	case "testnet2":
-		return DefaultTestnet2GenesisBlock()
-	case "testnet3":
-		return DefaultTestnet3GenesisBlock()
-	default:
-		log.Warn("unknown genesis block", "chain", name)
-		return nil
+var (
+	byName = map[string]*Genesis{
+		"aqua":     NewDefaultGenesisBlock(),
+		"testnet":  NewDefaultTestnetGenesisBlock(),
+		"testnet2": NewDefaultTestnet2GenesisBlock(),
+		"testnet3": NewDefaultTestnet3GenesisBlock(),
 	}
+)
+
+func DefaultGenesisByName(name string) *Genesis {
+	if g, ok := byName[name]; ok { // use byName to avoid creating a new genesis block
+		return g
+	}
+	log.Error("unknown genesis block", "chain", name)
+	return nil
+
 }
 
 // DefaultGenesisBlock returns the Aquachain main net genesis block.
-func DefaultGenesisBlock() *Genesis {
+func NewDefaultGenesisBlock() *Genesis {
+	log.Warn("Using default genesis block", "caller", log.Caller(1))
 	return &Genesis{
 		Config:     params.MainnetChainConfig,
 		Nonce:      42,
@@ -375,7 +378,7 @@ func DefaultGenesisBlock() *Genesis {
 }
 
 // DefaultTestnetGenesisBlock returns the Ropsten network genesis block.
-func DefaultTestnetGenesisBlock() *Genesis {
+func NewDefaultTestnetGenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.TestnetChainConfig,
 		Nonce:      66,
@@ -385,7 +388,7 @@ func DefaultTestnetGenesisBlock() *Genesis {
 }
 
 // DefaultTestnet2GenesisBlock returns the Testnet2 network genesis block.
-func DefaultTestnet2GenesisBlock() *Genesis {
+func NewDefaultTestnet2GenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.Testnet2ChainConfig,
 		Timestamp:  1492009146,
@@ -396,7 +399,7 @@ func DefaultTestnet2GenesisBlock() *Genesis {
 
 // DefaultTestnet3GenesisBlock returns the Testnet3 network genesis block.
 // This is a temporary chain for testing purposes ONLY.
-func DefaultTestnet3GenesisBlock() *Genesis {
+func NewDefaultTestnet3GenesisBlock() *Genesis {
 	m := decodePrealloc(allocData802018) // from mainnet block 802018
 
 	faucet := common.HexToAddress("0x615dc0d304ca8d97263db40b10968696c4828306")
@@ -422,7 +425,7 @@ func DefaultTestnet3GenesisBlock() *Genesis {
 
 // DeveloperGenesisBlock returns the 'aquachain --dev' genesis block. Note, this must
 // be seeded with the
-func DeveloperGenesisBlock(period uint64, faucet common.Address) *Genesis {
+func NewDeveloperGenesisBlock(period uint64, faucet common.Address) *Genesis {
 	// Override the default period to the user requested one
 	config := *params.AllCliqueProtocolChanges
 	// '-chain dev', disable pow, enable clique

core/genesis_test.go
@@ -35,7 +35,7 @@ import (
 // for generating delloc array for HF4 (only on mainnet)
 func TestDefaultGenesisAlloc(t *testing.T) {
 	t.SkipNow()
-	m := DefaultGenesisBlock().Alloc
+	m := NewDefaultGenesisBlock().Alloc
 	fmt.Println("Bad Balances:", len(m))
 	aquawei := big.NewFloat(params.Aqua)
 	_ = aquawei
@@ -50,20 +50,20 @@ func TestDefaultGenesisAlloc(t *testing.T) {
 		}
 	}
 }
-func TestDefaultGenesisBlock(t *testing.T) {
-	block := DefaultGenesisBlock().ToBlock(nil)
+func TestNewDefaultGenesisBlock(t *testing.T) {
+	block := NewDefaultGenesisBlock().ToBlock(nil)
 	if block.Hash() != params.MainnetGenesisHash {
 		t.Errorf("wrong mainnet genesis hash, got %x, want %x", block.Hash(), params.MainnetGenesisHash)
 	}
-	block = DefaultTestnetGenesisBlock().ToBlock(nil)
+	block = NewDefaultTestnetGenesisBlock().ToBlock(nil)
 	if block.Hash() != params.TestnetGenesisHash {
 		t.Errorf("wrong testnet genesis hash, got %x, want %x", block.Hash(), params.TestnetGenesisHash)
 	}
-	block = DefaultTestnet2GenesisBlock().ToBlock(nil)
+	block = NewDefaultTestnet2GenesisBlock().ToBlock(nil)
 	if block.Hash() != params.Testnet2GenesisHash {
 		t.Errorf("wrong testnet2 genesis hash, got %x, want %x", block.Hash(), params.Testnet2GenesisHash)
 	}
-	block = DefaultTestnet3GenesisBlock().ToBlock(nil)
+	block = NewDefaultTestnet3GenesisBlock().ToBlock(nil)
 	if block.Hash() != params.Testnet3GenesisHash {
 		t.Errorf("wrong testnet3 genesis hash, got %x, want %x", block.Hash(), params.Testnet3GenesisHash)
 	}
@@ -110,7 +110,7 @@ func TestSetupGenesis(t *testing.T) {
 		{
 			name: "mainnet block in DB, genesis == nil",
 			fn: func(db aquadb.Database) (*params.ChainConfig, common.Hash, error) {
-				DefaultGenesisBlock().MustCommit(db)
+				NewDefaultGenesisBlock().MustCommit(db)
 				return SetupGenesisBlock(db, nil)
 			},
 			wantHash:   params.MainnetGenesisHash,
@@ -129,7 +129,7 @@ func TestSetupGenesis(t *testing.T) {
 			name: "custom block in DB, genesis == testnet",
 			fn: func(db aquadb.Database) (*params.ChainConfig, common.Hash, error) {
 				customg.MustCommit(db)
-				return SetupGenesisBlock(db, DefaultTestnetGenesisBlock())
+				return SetupGenesisBlock(db, NewDefaultTestnetGenesisBlock())
 			},
 			wantErr:    &GenesisMismatchError{Stored: customghash, New: params.TestnetGenesisHash},
 			wantHash:   params.TestnetGenesisHash,
@@ -139,7 +139,7 @@ func TestSetupGenesis(t *testing.T) {
 			name: "custom block in DB, genesis == testnet2",
 			fn: func(db aquadb.Database) (*params.ChainConfig, common.Hash, error) {
 				customg.MustCommit(db)
-				return SetupGenesisBlock(db, DefaultTestnet2GenesisBlock())
+				return SetupGenesisBlock(db, NewDefaultTestnet2GenesisBlock())
 			},
 			wantErr:    &GenesisMismatchError{Stored: customghash, New: params.Testnet2GenesisHash},
 			wantHash:   params.Testnet2GenesisHash,
@@ -149,7 +149,7 @@ func TestSetupGenesis(t *testing.T) {
 			name: "custom block in DB, genesis == testnet3",
 			fn: func(db aquadb.Database) (*params.ChainConfig, common.Hash, error) {
 				customg.MustCommit(db)
-				return SetupGenesisBlock(db, DefaultTestnet3GenesisBlock())
+				return SetupGenesisBlock(db, NewDefaultTestnet3GenesisBlock())
 			},
 			wantErr:    &GenesisMismatchError{Stored: customghash, New: params.Testnet3GenesisHash},
 			wantHash:   params.Testnet3GenesisHash,

core/types/block.go
@@ -30,6 +30,7 @@ import (
 
 	"gitlab.com/aquachain/aquachain/common"
 	"gitlab.com/aquachain/aquachain/common/hexutil"
+	"gitlab.com/aquachain/aquachain/common/log"
 	"gitlab.com/aquachain/aquachain/crypto"
 	"gitlab.com/aquachain/aquachain/crypto/sha3"
 	"gitlab.com/aquachain/aquachain/params"
@@ -194,7 +195,11 @@ func rlpHash(version byte, x interface{}) (h common.Hash) {
 	switch version {
 	case 0, 1: // ethash
 		hw := sha3.NewKeccak256()
-		rlp.Encode(hw, x)
+		if err := rlp.Encode(hw, x); err != nil {
+			log.GracefulShutdownf("encountered error: %v", err)
+			time.Sleep(time.Second * 30) // hopefully doesn't return
+			panic("encountered error" + err.Error())
+		}
 		hw.Sum(h[:0])
 		return h
 	default: // argon2id switch

internal/debug/flags.go
@@ -59,7 +59,7 @@ var (
 	debugFlag = &cli.BoolFlag{
 		Name:  "debug",
 		Usage: "Prepends log messages with call-site location (file and line number)",
-		Value: sense.EnvBool("DEBUG"),
+		Value: sense.EnvBool("DEBUG") || sense.EnvBool("LOG_CALLERS"),
 	}
 	pprofFlag = &cli.BoolFlag{
 		Name:  "pprof",

node/config.go
@@ -225,10 +225,14 @@ func (c Config) HTTPEndpoint() string {
 	return getEndpoint(c.HTTPHost, c.HTTPPort)
 }
 
-// DefaultHTTPEndpoint returns the HTTP endpoint used by default.
-func DefaultHTTPEndpoint() string {
-	return (Config{HTTPHost: DefaultHTTPHost, HTTPPort: DefaultHTTPPort}).HTTPEndpoint()
-}
+// // DefaultHTTPEndpoint returns the HTTP endpoint used by default.
+// func DefaultHTTPEndpoint(chaincfg *params.ChainConfig) string {
+// 	defport := chaincfg.DefaultRpcPort
+// 	if defport == 0 {
+// 		log.Warn("chaincfg.DefaultHttpPort() returned 0, using random port")
+// 	}
+// 	return (Config{HTTPHost: DefaultHTTPHost, HTTPPort: defport}).HTTPEndpoint()
+// }
 
 // WSEndpoint resolves an websocket endpoint based on the configured host interface
 // and port parameters.
@@ -243,11 +247,11 @@ func getEndpoint(host string, port int) string {
 	return fmt.Sprintf("%s:%d", host, port)
 }
 
-// DefaultWSEndpoint returns the websocket endpoint used by default.
-func DefaultWSEndpoint() string {
-	config := &Config{WSHost: DefaultWSHost, WSPort: DefaultWSPort}
-	return config.WSEndpoint()
-}
+// // DefaultWSEndpoint returns the websocket endpoint used by default.
+// func DefaultWSEndpoint() string {
+// 	config := &Config{WSHost: DefaultWSHost, WSPort: DefaultWSPort}
+// 	return config.WSEndpoint()
+// }
 
 func (c *Config) NodeName() string {
 	return GetNodeName(c)

node/defaults.go
@@ -30,9 +30,9 @@ import (
 
 const (
 	DefaultHTTPHost = "127.0.0.1" // Default host interface for the HTTP RPC server
-	DefaultHTTPPort = 8543        // Default TCP port for the HTTP RPC server
-	DefaultWSHost   = "127.0.0.1" // Default host interface for the websocket RPC server
-	DefaultWSPort   = 8544        // Default TCP port for the websocket RPC server
+	// DefaultHTTPPort = 8543        // Default TCP port for the HTTP RPC server
+	DefaultWSHost = "127.0.0.1" // Default host interface for the websocket RPC server
+	// DefaultWSPort   = 8544        // Default TCP port for the websocket RPC server
 )
 
 // DefaultConfig contains reasonable default settings.
@@ -41,21 +41,21 @@ const (
 func NewDefaultConfig() *Config {
 	datadir := defaultDataDir()
 	x := &Config{
-		Name:        "", // must be set before GetNodeName
-		DataDir:     datadir,
-		HTTPPort:    DefaultHTTPPort,
+		Name:    "", // must be set before GetNodeName
+		DataDir: datadir,
+		// HTTPPort:    DefaultHTTPPort,
 		HTTPModules: []string{"aqua", "eth", "net", "web3"},
-		WSPort:      DefaultWSPort,
-		WSModules:   []string{"aqua", "eth", "net", "web3"},
+		// WSPort:      DefaultWSPort,
+		WSModules: []string{"aqua", "eth", "net", "web3"},
 		P2P: &p2p.Config{
-			ListenAddr: "0.0.0.0:21303", // tcp+udp, ipv4 only
+			ListenAddr: "0.0.0.0:24242", // tcp+udp, ipv4 only. this is modified by node.NewNode
 			MaxPeers:   20,
 			NAT:        "none", // none
 		},
 		RPCBehindProxy: sense.EnvBool("RPC_BEHIND_PROXY"),
 		UserIdent:      sense.Getenv("AQUA_USERIDENT"),
-		HTTPHost:       "",
-		WSHost:         "",
+		HTTPHost:       "", // no rpc
+		WSHost:         "", // no ws
 		KeyStoreDir:    sense.Getenv("AQUA_KEYSTORE_DIR"),
 	}
 	return x

opt/console/console_test.go
@@ -124,7 +124,7 @@ func newTester(t *testing.T, confOverride func(*aqua.Config)) *tester {
 		t.Fatalf("failed to create node: %v", err)
 	}
 
-	genesis := core.DeveloperGenesisBlock(15, common.Address{})
+	genesis := core.NewDeveloperGenesisBlock(15, common.Address{})
 	genesis.Config = chaincfg // inject chaincfg into genesis
 	ethConf := &aqua.Config{
 		Genesis:  genesis,

commit b90282b8f6fd17463b6ee4781dbe88d6045ff191

fix bug introduced in previous commit   5a1deaefa569f2332346cf105c2c6d6f338885df

rlp/encode.go
@@ -111,7 +111,7 @@ func EncodeToBytes(val interface{}) ([]byte, error) {
 // Please see the documentation of Encode for the encoding rules.
 func EncodeToReader(val interface{}) (size int, r io.Reader, err error) {
 	eb := encbufPool.Get().(*encbuf)
-	defer encbufPool.Put(eb)
+	// defer encbufPool.Put(eb) // already done by encReader
 	eb.reset()
 	if err := eb.encode(val); err != nil {
 		return 0, nil, err